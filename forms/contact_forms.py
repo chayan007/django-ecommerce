21be004a95798cdef9b2d099024fd2f5efb974cc
Chayan Datta <sonicxxx7@gmail.com>
2019-04-13 12:35:45 +0530

    contact forms done

--- forms/contact_forms.py
+++ forms/contact_forms.py
@@ -2,6 +2,33 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
-    content = forms.CharField()
+    name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter the Name"
+            }
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter the Name"
+            }
+        )
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter the Name"
+            }
+        )
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not "gmail.com" in email:
+            raise forms.ValidationError("You must have a GMail address")
+        return email
