3c6d54699c3d0fe78e3b25d3103a7595bfe19ef9
Chayan Datta <sonicxxx7@gmail.com>
2019-04-14 14:33:12 +0530

    Login logic done

--- forms/auth_forms.py
+++ forms/auth_forms.py
@@ -2,7 +2,19 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter the Username"
+            }
+        )
+    )
     password = forms.CharField(
-        widget=forms.PasswordInput
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter the Password"
+            }
+        )
     )
