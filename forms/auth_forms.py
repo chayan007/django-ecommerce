9ea261bef6d2b17a0b5c70bed84121fa9b3664ce
Chayan Datta <sonicxxx7@gmail.com>
2019-04-14 14:38:01 +0530

    Register form boilerplate done

--- forms/auth_forms.py
+++ forms/auth_forms.py
@@ -18,3 +18,38 @@ class LoginForm(forms.Form):
             }
         )
     )
+
+
+class RegisterForm(forms.Form):
+    name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter the Name"
+            }
+        )
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter the Name"
+            }
+        )
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter the Username"
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter the Password"
+            }
+        )
+    )
