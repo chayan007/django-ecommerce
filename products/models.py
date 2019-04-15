c2be6f971c69dd05cfea0f7192ed0fd2def8914a
Chayan Datta <sonicxxx7@gmail.com>
2019-04-15 10:09:30 +0530

    Product Model staged

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    title = models.CharField()
+    description = models.CharField()
+    price = models.CharField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
