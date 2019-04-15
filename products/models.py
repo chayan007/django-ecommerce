ac378b8e86b937c323f2457f347fecff6ceeb735
Chayan Datta <sonicxxx7@gmail.com>
2019-04-15 11:16:19 +0530

    Product List view done

--- products/models.py
+++ products/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 
 class Products(models.Model):
-    title = models.CharField()
-    description = models.CharField()
-    price = models.CharField()
-    image = models.ImageField()
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    image = models.ImageField(upload_to='/uploads')
 
     def __str__(self):
+        return self.title
+
+    def __unicode__(self):
         return self.title
--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Products
 
 # Create your views here.
+
+
+class ProductListView(ListView):
+    queryset = Products.objects.all()
+    template_name = 'products/product_list.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        return context
+
+
