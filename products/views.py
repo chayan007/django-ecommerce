ae1349f0e81213c0794d27ca6ac8765512bc8b62
Chayan Datta <sonicxxx7@gmail.com>
2019-04-17 14:33:00 +0530

    Pillow installed and detailed view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Products
 
 # Create your views here.
@@ -13,4 +13,11 @@ class ProductListView(ListView):
         context = super(ProductListView, self).get_context_data(**kwargs)
         return context
 
+
+class ProductDetailView(DetailView):
+    model = Products
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
